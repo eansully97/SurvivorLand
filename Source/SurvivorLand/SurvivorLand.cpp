2369efb2b0d608345eeda527bbb1f63d60ae53c2
eansu <eansullivan@yahoo.com>
2026-03-02 09:47:58 -0600

    Init

--- Source/SurvivorLand/SurvivorLand.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SurvivorLand.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SurvivorLand, "SurvivorLand" );
-
-DEFINE_LOG_CATEGORY(LogSurvivorLand)
